0aba4125bae055dd9571d9b480461c1155f7ef05
shonansurvivors <shonansurvivors@gmail.com>
2019-01-13 03:39:04 +0900

    cache

--- trip_app/urls.py
+++ trip_app/urls.py
@@ -3,7 +3,7 @@ from django.views.decorators.cache import cache_page
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', cache_page(3600 * 24)(views.IndexView.as_view()), name='index'),
     path('article/', cache_page(3600 * 24)(views.ArticleListView.as_view()), name='article_list'),
     path('area/<str:area>/', cache_page(3600 * 24)(views.AreaCountryListView.as_view()), name='area_country_list'),
     path('country/<str:name>/', views.CountryHeritageListView.as_view(), name='country_heritage_list'),
